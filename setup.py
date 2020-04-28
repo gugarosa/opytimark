bd118a04282b309c249a5c4b500af41c36d6c904
Gustavo Rosa <gth.rosa@uol.com.br>
2020-04-28 11:24:56 -0300

    Adding initial files.

---
+++ setup.py
@@ -0,0 +1,38 @@
+from setuptools import find_packages, setup
+
+with open('README.md', 'r') as f:
+    long_description = f.read()
+
+setup(name='opytimark',
+      version='1.0.0',
+      description='Python Optimization Benchmarking Functions',
+      long_description=long_description,
+      long_description_content_type='text/markdown',
+      author='Gustavo Rosa',
+      author_email='gth.rosa@uol.com.br',
+      url='https://github.com/gugarosa/opytimark',
+      license='GPL-3.0',
+      install_requires=['coverage>=5.0.3',
+                        'numpy>=1.18.1',
+                        'pylint>=2.4.4',
+                        'pytest>=5.3.3'
+                        ],
+      extras_require={
+          'tests': ['coverage',
+                    'pytest',
+                    'pytest-pep8',
+                    ],
+      },
+      classifiers=[
+          'Development Status :: 5 - Production/Stable',
+          'Intended Audience :: Developers',
+          'Intended Audience :: Education',
+          'Intended Audience :: Science/Research',
+          'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+          'Programming Language :: Python :: 3.6',
+          'Programming Language :: Python :: 3.7',
+          'Programming Language :: Python :: 3.8',
+          'Topic :: Software Development :: Libraries',
+          'Topic :: Software Development :: Libraries :: Python Modules'
+      ],
+      packages=find_packages())
