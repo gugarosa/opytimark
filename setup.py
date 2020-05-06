08614c9b22390af9205da7eb79543ab7f9f608cd
Gustavo Rosa <gth.rosa@uol.com.br>
2020-05-06 17:02:56 -0300

    Bumping version for future release.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open('README.md', 'r') as f:
     long_description = f.read()
 
 setup(name='opytimark',
-      version='1.0.1',
+      version='1.0.2',
       description='Python Optimization Benchmarking Functions',
       long_description=long_description,
       long_description_content_type='text/markdown',
