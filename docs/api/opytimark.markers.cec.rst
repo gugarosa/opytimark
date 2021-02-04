49bae38433fc99a32601b08a4d648d448bed3b09
Gustavo Rosa <gth.rosa@uol.com.br>
2021-02-04 12:03:05 -0300

    Adding missing tests.

--- docs/api/opytimark.markers.cec.rst
+++ docs/api/opytimark.markers.cec.rst
@@ -3,3 +3,7 @@ opytimark.markers.cec
 
 .. toctree::
    opytimark.markers.cec.year_2005
+
+.. autoapimodule:: opytimark.markers.cec
+   :members:
+   :show-inheritance:
---
+++ tests/opytimark/utils/test_loader.py
@@ -0,0 +1,28 @@
+import os
+
+from opytimark.utils import loader
+
+
+def test_download_file():
+    url = 'http://recogna.tech/files/opytimark/'
+    tar_name = '2005.tar.gz'
+    tar_path = f'data/{tar_name}'
+
+    loader.download_file(url + tar_name, tar_path)
+
+    assert os.path.exists(tar_path)
+
+
+def test_untar_file():
+    tar_name = '2005.tar.gz'
+    tar_path = f'data/{tar_name}'
+
+    folder_path = loader.untar_file(tar_path)
+
+    assert os.path.exists(folder_path)
+
+
+def test_load_cec_auxiliary():
+    data = loader.load_cec_auxiliary('F1', '2005')
+
+    assert data.shape[0] == 100
