8e08458fe63b78fae5a0051bfe69d3f25864836d
Gustavo Rosa <gth.rosa@uol.com.br>
2021-02-05 14:44:51 -0300

    New CEC example seems to be handable now.

--- opytimark/markers/cec/year_2005.py
+++ opytimark/markers/cec/year_2005.py
@@ -62,3 +62,70 @@ class F1(Benchmark):
         f = z ** 2
 
         return np.sum(f) - 450
+
+
+class F3(Benchmark):
+    """F3 class implements the Shifted Rotated High Conditioned Elliptic's benchmarking function.
+
+    .. math:: f(\mathbf{x}) = f(x_1, x_2, \ldots, x_n) = \sum_{i=1}^{n} z_i^2 - 450 \mid z_i = x_i - o_i
+
+    Domain:
+        The function is commonly evaluated using :math:`x_i \in [-100, 100] \mid i = \{1, 2, \ldots, n\}, n \leq 100`.
+
+    Global Minima:
+        :math:`f(\mathbf{x^*}) = -450 \mid \mathbf{x^*} = \mathbf{o}`.
+
+    """
+
+    def __init__(self, name='F3', dims=-1, continuous=True, convex=True,
+                 differentiable=True, multimodal=False, separable=False):
+        """Initialization method.
+
+        Args:
+            name (str): Name of the function.
+            dims (int): Number of allowed dimensions.
+            continuous (bool): Whether the function is continuous.
+            convex (bool): Whether the function is convex.
+            differentiable (bool): Whether the function is differentiable.
+            multimodal (bool): Whether the function is multimodal.
+            separable (bool): Whether the function is separable.
+
+        """
+
+        # Override its parent class
+        super(F3, self).__init__(name, dims, continuous,
+                                 convex, differentiable, multimodal, separable)
+
+        # Loads auxiliary data and define it as a property
+        self.o = l.load_cec_auxiliary('F3', '2005')
+
+        # Pre-loads every auxiliary matrix for faster computing
+        self.M_2 = l.load_cec_auxiliary('F3_D2', '2005')
+        self.M_10 = l.load_cec_auxiliary('F3_D10', '2005')
+        self.M_30 = l.load_cec_auxiliary('F3_D30', '2005')
+        self.M_50 = l.load_cec_auxiliary('F3_D50', '2005')
+
+    @d.check_exact_dimension_and_auxiliary_matrix
+    def __call__(self, x):
+        """This method returns the function's output when the class is called.
+
+        Args:
+            x (np.array): An input array for calculating the function's output.
+
+        Returns:
+            The benchmarking function output `f(x)`.
+
+        """
+
+        # Re-calculates the input
+        z = np.matmul(x - self.o[:x.shape[0]], self.M)
+
+        # Instantiating function
+        f = 0
+
+        # For every input dimension
+        for i in range(x.shape[0]):
+            # Calculating the Shifted Rotated High Conditioned Elliptic's function
+            f += 10e6 ** (i / (x.shape[0] - 1)) * z[i] ** 2
+
+        return f - 450
--- opytimark/utils/decorator.py
+++ opytimark/utils/decorator.py
@@ -49,12 +49,70 @@ def check_exact_dimension(f):
         # If the input dimensions is different from function's allowed dimensions
         if x.shape[0] != obj.dims:
             # Raises an error
-            raise e.SizeError(
-                f'{obj.name} input should be {obj.dims}-dimensional')
+            raise e.SizeError(f'{obj.name} input should be {obj.dims}-dimensional')
 
         return f(obj, x)
 
     return _check_exact_dimension
+
+
+def check_exact_dimension_and_auxiliary_matrix(f):
+    """Checks whether the input dimension is exact to the demanded by the evaluated function and defines
+    a proper auxiliary matrix accordingly.
+
+    Args:
+        f (callable): Function to be checked.
+
+    Returns:
+        The function output or an error depending whether the check is valid.
+
+    """
+
+    def _check_exact_dimension_and_auxiliary_matrix(*args):
+        """Wraps the dimension checking in order to provide additional logic.
+
+        Returns:
+            The wrapped function output.
+
+        """
+
+        # Retrieving the object and the input from arguments
+        obj, x = args[0], args[1]
+
+        # Tries to squeeze the last dimension of `x` as it might be an array of (dim, 1)
+        try:
+            # Squeezes the array
+            x = np.squeeze(x, axis=1)
+
+        # If squeeze could not be performed, it means that there is no extra dimension
+        except ValueError:
+            pass
+
+        # If the input dimensions differs from function's allowed dimensions
+        if x.shape[0] not in [2, 10, 30, 50]:
+            # Raises an error
+            raise e.SizeError(f'{obj.name} input should be 2-, 10-, 30- or 50-dimensional')
+
+        # If the input dimension is equal to 2
+        if x.shape[0] == 2:
+            # Defines the auxiliary matrix `M` as `M_2`
+            setattr(obj, 'M', obj.M_2)
+        # If the input dimension is equal to 10
+        elif x.shape[0] == 10:
+            # Defines the auxiliary matrix `M` as `M_10`
+            setattr(obj, 'M', obj.M_10)
+        # If the input dimension is equal to 30
+        elif x.shape[0] == 30:
+            # Defines the auxiliary matrix `M` as `M_30`
+            setattr(obj, 'M', obj.M_30)
+        # If the input dimension is equal to 50
+        elif x.shape[0] == 50:
+            # Defines the auxiliary matrix `M` as `M_50`
+            setattr(obj, 'M', obj.M_50)
+
+        return f(obj, x)
+
+    return _check_exact_dimension_and_auxiliary_matrix
 
 
 def check_less_equal_dimension(f):
@@ -91,8 +149,7 @@ def check_less_equal_dimension(f):
         # If the input dimensions is different from function's allowed dimensions
         if x.shape[0] > obj.dims:
             # Raises an error
-            raise e.SizeError(
-                f'{obj.name} input should be less or equal to {obj.dims}-dimensional')
+            raise e.SizeError(f'{obj.name} input should be less or equal to {obj.dims}-dimensional')
 
         return f(obj, x)
 
