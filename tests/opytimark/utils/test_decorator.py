4bcecbcd90778e5bb4fcea4cd52de65295d1b5a8
Gustavo Rosa <gth.rosa@uol.com.br>
2021-02-05 14:18:19 -0300

    Adding unitary test for newly implemented decorator.

--- tests/opytimark/utils/test_decorator.py
+++ tests/opytimark/utils/test_decorator.py
@@ -1,6 +1,7 @@
 import numpy as np
 
 from opytimark.markers import n_dimensional
+from opytimark.markers.cec import year_2005
 from opytimark.utils import decorator
 
 
@@ -22,3 +23,16 @@ def test_check_exact_dimension():
         call(f, np.array([1, 2]))
     except:
         call(f, np.array([1]))
+
+
+def test_check_less_equal_dimension():
+    @decorator.check_less_equal_dimension
+    def call(obj, x):
+        return x
+
+    f = year_2005.F1()
+
+    try:
+        call(f, np.zeros(101))
+    except:
+        call(f, np.zeros(100))
