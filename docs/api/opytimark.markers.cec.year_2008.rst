5114b0fe1099252e7597b4eb638d3a913bef038b
Gustavo Rosa <gth.rosa@uol.com.br>
2021-02-26 17:15:35 -0300

    Adding CEC2008 F2, F3 and F4.

--- docs/api/opytimark.markers.cec.year_2008.rst
+++ docs/api/opytimark.markers.cec.year_2008.rst
@@ -4,3 +4,15 @@ opytimark.markers.cec.year_2008
 .. autoapiclass:: opytimark.markers.cec.year_2008.F1
    :members:
    :show-inheritance:
+
+.. autoapiclass:: opytimark.markers.cec.year_2008.F2
+   :members:
+   :show-inheritance:
+
+.. autoapiclass:: opytimark.markers.cec.year_2008.F3
+   :members:
+   :show-inheritance:
+
+.. autoapiclass:: opytimark.markers.cec.year_2008.F4
+   :members:
+   :show-inheritance:
--- opytimark/markers/cec/year_2008.py
+++ opytimark/markers/cec/year_2008.py
@@ -60,3 +60,173 @@ class F1(CECBenchmark):
         f = z ** 2
 
         return np.sum(f) - 450
+
+
+class F2(CECBenchmark):
+    """F2 class implements the Shifted Schwefel's 2.21 benchmarking function.
+
+    .. math:: f(\mathbf{x}) = f(x_1, x_2, \ldots, x_n) = \max_{i=1, \ldots, n}|z_i| - 450 \mid z_i = x_i - o_i
+
+    Domain:
+        The function is commonly evaluated using :math:`x_i \in [-100, 100] \mid i = \{1, 2, \ldots, n\}, n \leq 1000`.
+
+    Global Minima:
+        :math:`f(\mathbf{x^*}) = -450 \mid \mathbf{x^*} = \mathbf{o}`.
+
+    """
+
+    def __init__(self, name='F2', year='2008', auxiliary_data=('o'), dims=1000,
+                 continuous=True, convex=True, differentiable=True, multimodal=False, separable=False):
+        """Initialization method.
+
+        Args:
+            name (str): Name of the function.
+            year (str): Year of the function.
+            auxiliary_data (tuple): Auxiliary variables to be externally loaded.
+            dims (int): Number of allowed dimensions.
+            continuous (bool): Whether the function is continuous.
+            convex (bool): Whether the function is convex.
+            differentiable (bool): Whether the function is differentiable.
+            multimodal (bool): Whether the function is multimodal.
+            separable (bool): Whether the function is separable.
+
+        """
+
+        # Override its parent class
+        super(F2, self).__init__(name, year, auxiliary_data, dims, continuous,
+                                 convex, differentiable, multimodal, separable)
+
+    @d.check_less_equal_dimension
+    def __call__(self, x):
+        """This method returns the function's output when the class is called.
+
+        Args:
+            x (np.array): An input array for calculating the function's output.
+
+        Returns:
+            The benchmarking function output `f(x)`.
+
+        """
+
+        # Re-calculates the input
+        z = x - self.o[:x.shape[0]]
+
+        # Calculating the Schwefel's 2.21 function
+        f = np.fabs(z)
+
+        return np.amax(f) - 450
+
+
+class F3(CECBenchmark):
+    """F3 class implements the Shifted Rosenbrock's benchmarking function.
+
+    .. math:: f(\mathbf{x}) = f(x_1, x_2, \ldots, x_n) = \sum_{i=1}^{n-1} (100(z_i^2-z_{i+1})^2 + (z_i - 1)^2) + 390 \mid z_i = x_i - o_i
+
+    Domain:
+        The function is commonly evaluated using :math:`x_i \in [-100, 100] \mid i = \{1, 2, \ldots, n\}, n \leq 1000`.
+
+    Global Minima:
+        :math:`f(\mathbf{x^*}) = -390 \mid \mathbf{x^*} = \mathbf{o}`.
+
+    """
+
+    def __init__(self, name='F3', year='2008', auxiliary_data=('o'), dims=100,
+                 continuous=True, convex=True, differentiable=True, multimodal=True, separable=False):
+        """Initialization method.
+
+        Args:
+            name (str): Name of the function.
+            year (str): Year of the function.
+            auxiliary_data (tuple): Auxiliary variables to be externally loaded.
+            dims (int): Number of allowed dimensions.
+            continuous (bool): Whether the function is continuous.
+            convex (bool): Whether the function is convex.
+            differentiable (bool): Whether the function is differentiable.
+            multimodal (bool): Whether the function is multimodal.
+            separable (bool): Whether the function is separable.
+
+        """
+
+        # Override its parent class
+        super(F3, self).__init__(name, year, auxiliary_data, dims, continuous,
+                                 convex, differentiable, multimodal, separable)
+
+    @d.check_less_equal_dimension
+    def __call__(self, x):
+        """This method returns the function's output when the class is called.
+
+        Args:
+            x (np.array): An input array for calculating the function's output.
+
+        Returns:
+            The benchmarking function output `f(x)`.
+
+        """
+
+        # Re-calculates the input
+        z = x - self.o[:x.shape[0]] + 1
+
+        # Instantiating function
+        f = 0
+
+        # For every input dimension
+        for i in range(x.shape[0] - 1):
+            # Calculating the Shifted Rosenbrock's function
+            f += (100 * (z[i] ** 2 - z[i+1]) ** 2 + (z[i] - 1) ** 2)
+
+        return f + 390
+
+
+class F4(CECBenchmark):
+    """F4 class implements the Shifted Rastrigin's benchmarking function.
+
+    .. math:: f(\mathbf{x}) = f(x_1, x_2, \ldots, x_n) = \sum_{i=1}^{n} (z_i^2 - 10cos(2 \\pi z_i) + 10) - 330 \mid z_i = x_i - o_i
+
+    Domain:
+        The function is commonly evaluated using :math:`x_i \in [-5, 5] \mid i = \{1, 2, \ldots, n\}, n \leq 1000`.
+
+    Global Minima:
+        :math:`f(\mathbf{x^*}) = -330 \mid \mathbf{x^*} = \mathbf{o}`.
+
+    """
+
+    def __init__(self, name='F4', year='2008', auxiliary_data=('o'), dims=100,
+                 continuous=True, convex=True, differentiable=True, multimodal=True, separable=True):
+        """Initialization method.
+
+        Args:
+            name (str): Name of the function.
+            year (str): Year of the function.
+            auxiliary_data (tuple): Auxiliary variables to be externally loaded.
+            dims (int): Number of allowed dimensions.
+            continuous (bool): Whether the function is continuous.
+            convex (bool): Whether the function is convex.
+            differentiable (bool): Whether the function is differentiable.
+            multimodal (bool): Whether the function is multimodal.
+            separable (bool): Whether the function is separable.
+
+        """
+
+        # Override its parent class
+        super(F4, self).__init__(name, year, auxiliary_data, dims, continuous,
+                                 convex, differentiable, multimodal, separable)
+
+    @d.check_less_equal_dimension
+    def __call__(self, x):
+        """This method returns the function's output when the class is called.
+
+        Args:
+            x (np.array): An input array for calculating the function's output.
+
+        Returns:
+            The benchmarking function output `f(x)`.
+
+        """
+
+        # Re-calculates the input
+        z = x - self.o[:x.shape[0]]
+
+        # Calculating the Shifted Rastrigin's function
+        f = z ** 2 - 10 * np.cos(2 * np.pi * z) + 10
+
+        return np.sum(f) - 330
