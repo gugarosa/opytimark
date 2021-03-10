Fixing up elliptic-based calculation.

opytimark/markers/cec/year_2005.py
@@ -180,18 +180,17 @@ class F3(CECBenchmark):
 
         """
 
+        # Defines the number of dimensions and an equally-spaced interval between 0 and D-1
+        D = x.shape[0]
+        dims = np.linspace(1, D, D) - 1
+
         # Re-calculates the input
-        z = np.matmul(x - self.o[:x.shape[0]], self.M)
+        z = np.matmul(x - self.o[:D], self.M)
 
-        # Instantiating function
-        f = 0
+        # Calculating the Shifted Rotated High Conditioned Elliptic's function
+        z = 10e6 ** (dims / (D - 1)) * z ** 2
 
-        # For every input dimension
-        for i in range(x.shape[0]):
-            # Calculating the Shifted Rotated High Conditioned Elliptic's function
-            f += 10e6 ** (i / (x.shape[0] - 1)) * z[i] ** 2
-
-        return f - 450
+        return np.sum(z) - 450
 
 
 class F4(CECBenchmark):

opytimark/markers/cec/year_2010.py
@@ -58,18 +58,17 @@ class F1(CECBenchmark):
 
         """
 
+        # Defines the number of dimensions and an equally-spaced interval between 0 and D-1
+        D = x.shape[0]
+        dims = np.linspace(1, D, D) - 1
+
         # Re-calculates the input
-        z = x - self.o[:x.shape[0]]
+        z = x - self.o[:D]
 
-        # Instantiating function
-        f = 0
+        # Calculating the Shifted Elliptic's function
+        z = 10e6 ** (dims / (D - 1)) * z ** 2
 
-        # For every input dimension
-        for i in range(x.shape[0]):
-            # Calculating the Shifted Elliptic's function
-            f += 10e6 ** (i / (x.shape[0] - 1)) * z[i] ** 2
-
-        return f
+        return np.sum(z)
 
 
 class F2(CECBenchmark):

opytimark/markers/n_dimensional.py
@@ -883,15 +883,13 @@ class HighConditionedElliptic(Benchmark):
 
         """
 
-        # Instantiates the function
-        f = 0
+        # Calculates an equally-spaced interval between 0 and D-1
+        dims = np.linspace(1, x.shape[0], x.shape[0]) - 1
 
-        # Iterates through every dimension
-        for i in range(x.shape[0]):
-            # Calculates the High Conditioned Elliptic's function
-            f += (10e6) ** ((i-2) / (x.shape[0] - 1)) * x[i] ** 2
+        # Calculating the HighConditionedElliptic's function
+        x = 10e6 ** (dims / (x.shape[0] - 1)) * x ** 2
 
-        return f
+        return np.sum(x)
 
 
 class Levy(Benchmark):